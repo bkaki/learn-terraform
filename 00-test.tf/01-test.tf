8a0b6641ddf6adb98465c336939c7f1425261577
bkaki <bhaskarkakii77@gmail.com>
2023-04-27 08:44:48 +0530

    terraform

--- 00-test.tf/01-test.tf
+++ 00-test.tf/01-test.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "workstation" {
-  ami           = ami-0b5a2b5b8f2be4ec2
+  ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
@@ -7,6 +7,8 @@ resource "aws_instance" "workstation" {
   }
 }
 
- outpout "workstation" {
-   value = aws_instance.workstation.public_ip
- }
+output "workstation" {
+  value = aws_instance.workstation.public_ip
+}
+
+
