132497364852a706e0c951312445e290ed3b9315
bkaki <bhaskarkakii77@gmail.com>
2023-04-27 07:58:26 +0530

    terraform

--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -1,9 +1,22 @@
+# How to declare a variable
+
 variable "sample_string" {
   default = "Hello World"
 
 }
 
+# shell scripting equivalent sample_string ”Hello World”
+# Terraform we have to write in this way by using syntaxes
+#To print in shell script we use echo command to print
+# echo $sample_string
+
 output "sample_string" {
   value = var.sample_string
 
 }
+
+# In above case we are just printing the variable, if the variable is a combinations of some strings then following in the syntax
+
+ output "sample_string" {
+   value = "value of sample_string" = ${var.sample_string}
+ }
