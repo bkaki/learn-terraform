d35e5d2b351195c9a1b0943beda4033b52a06f21
bkaki <bhaskarkakii77@gmail.com>
2023-04-27 07:37:11 +0530

    terraform

--- ex1/vars.tf/vars.tf
+++ ex1/vars.tf/vars.tf
@@ -1,17 +1,17 @@
 # How to declare a variable
 
-output "sample_string" {
+variable "sample_string" {
   default = "Hello world"
 
 }
 
 ### shell scripting equivalent sample_string ”Hello World”
-Terraform we have to write in this way by using syntaxes
+# Terraform we have to write in this way by using syntaxes
 
-To print in shell script we use echo command to print
+# To print in shell script we use echo command to print
 
 # echo $sample_string
 
-Output “sample_string” {
+Output "sample_string" {
 Value = var.sample_string
 }
