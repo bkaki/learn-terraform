926204c9a326ce29e7c2c155c53ad11b9d402a0a
bkaki <bhaskarkakii77@gmail.com>
2023-05-02 18:16:48 +0530

    terraform

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -2,12 +2,12 @@
 resource "null_resource" "fruits" {
   count = length(var.fruits)
 
-#  provisioner "local-exec" {
-#  command = "echo Fruit Name -  ${var.fruits[count.index]}"
-#   // command = "echo ${var.fruits[count.index]}"
-#  }
-#
-#}
+  provisioner "local-exec" {
+  command = "echo Fruit Name -  ${var.fruits[count.index]}"
+   // command = "echo ${var.fruits[count.index]}"
+  }
+
+}
 
 variable "fruits" {
   default = [ "apple", "banana", "orange"]
