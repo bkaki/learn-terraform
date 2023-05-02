8a1bb9090b8bface84ebcd8c4ce3e60423222980
bkaki <bhaskarkakii77@gmail.com>
2023-05-02 18:17:39 +0530

    terraform

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -7,12 +7,13 @@ resource "null_resource" "fruits" {
 
 }
 
+variable "fruits" {
+  default = [ "apple", "banana", "orange"]
+}
+
 #  provisioner "local-exec" {
 #    command = "echo Fruit Name -  ${var.fruits[count.index]}"
 #  }
 
 #}
 
-variable "fruits" {
-  default = [ "apple", "banana", "orange"]
-}
