6960cb25aebcc50f81787d33270691a25f89d32a
bkaki <bhaskarkakii77@gmail.com>
2023-05-03 15:50:44 +0530

    terraform

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -1,15 +1,62 @@
-
 resource "null_resource" "fruits" {
-  count = length(var.fruits)
+
+  for_each = var.fruits
 
   provisioner "local-exec" {
-  command = "echo Fruit Name -  ${var.fruits[count.index]}"
-   // command = "echo ${var.fruits[count.index]}"
+    command = "echo Fruit Name - ${each.key} - ${each.value}"
+    //command = "echo  ${length(var.fruits)}"
   }
 
 }
 
-variable "fruits" {
-  default = [ "apple", "banana", "orange"]
+resource "null_resource" "fruits1" {
+
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.value["name"]} - ${each.value["count"]}"
+    //command = "echo  ${length(var.fruits)}"
+  }
+
 }
 
+variable "fruits1" {
+  default = {
+    apple = {
+      name  = "apple"
+      count = 10
+    }
+    orange = {
+      name  = "orange"
+      count = 200
+    }
+    banana = {
+      name  = "banana"
+      count = 100
+    }
+  }
+}
+
+variable "fruits" {
+  default = {
+    apple   = 10
+    banana  = 100
+    oranges = 200
+  }
+}
+
+
+// For-each on a list
+variable "vegetables" {
+  default = ["carrot ", "capsicum"]
+}
+
+resource "null_resource" "vegetables" {
+
+  for_each = toset(var.vegetables)
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key}"
+  }
+
+}
